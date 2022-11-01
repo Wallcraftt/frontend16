37902fa247437691c140413cb515847100e7880a
Wallcraft <102182020+Wallcraftt@users.noreply.github.com>
2022-11-01 19:19:31 +0400

    .

---
+++ js1/main.js
@@ -0,0 +1,35 @@
+// task 1)
+
+function getSum(a = 1, b = 2){
+  console.log( a + b,false)
+  console.log( b * 2 ,true)
+}
+
+getSum()
+
+/// task 2)
+
+function getSum2(a, b){
+  console.log(2*a + 2*b)
+
+  return 2 * a + 2* b ;
+}
+
+getSum2()
+
+
+/// task 3)
+
+function getSum3(a) {
+ console.log ( a * 4 )
+
+ return a * 4;
+ 
+}
+getSum3()
+
+// task 4) 
+
+
+
+// task 5)
